6cc694519ff38ec53d90d17a58fc0d2422c967c8
dharanakneu <kashyap.dh@northeastern.edu>
2025-01-12 00:26:58 -0500

    Add Hero and About section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import profilePic from '../assets/kevinRushProfile.png'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className={`relative w-full h-screen mx-auto`}>
+      <div
+        className={`absolute inset-0 top-[120px] lg:top-[80px] max-w-7xl mx-auto ${styles.paddingX} flex flex-col lg:flex-row items-start lg:items-center gap-5`}
+      >
+        <div className="flex flex-row items-start gap-5">
+          <div className='flex flex-col justify-center items-center mt-5'>
+            <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
+            <div className='w-1 h-40 lg:h-80 violet-gradient' />
+          </div>
+
+          <div>
+            <h1 className={`${styles.heroHeadText} text-white`}>
+              Hi, I'm <span className='text-[#915EFF]'>Dharana</span>
+            </h1>
+            <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+              I develop 3D visuals, user <br className='lg:block hidden' />
+              interfaces and web applications
+            </p>
+          </div>
+        </div>
+
+        <div className="flex w-full justify-center lg:w-1/2 mt-5 md:mt-0 lg:mt-0">
+          <img
+                src={profilePic}
+                alt="Dharana Kashyap"
+                className="w-full max-w-sm lg:max-w-md"
+              />
+        </div>
+      </div>
+
+      
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+      </div>
+    </section>
   )
 }
 
