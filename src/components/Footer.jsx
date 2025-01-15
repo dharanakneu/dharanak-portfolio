6a8ede34841ffb844ec498eca15b6e14d5211942
dharanakneu <kashyap.dh@northeastern.edu>
2025-01-14 19:55:04 -0500

    Add a footer section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-primary text-white py-6">
+      <div className="container mx-auto px-4">
+        <div className="text-center mt-6 text-sm text-gray-500">
+          © 2025 Dharana Kashyap. All rights reserved.
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
